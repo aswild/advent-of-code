7cb4585f0ac6a66a5fe50e0344af4c4b2739b426
Allen Wild <allenwild93@gmail.com>
2020-12-24 17:08:08 -0600

    day 23: rewrite to be faster
    Here we see the introduction of my patented Cursed List - a circular
    doubly linked list stored as a flat array with O(1) element lookup!
    
    Using an actual CDLL worked just fine and made sense, but was too slow
    for doing 10 million iterations in Part 2. But conveniently the values
    are all sequential integers, so we can use that label value as the index
    in a flat array.

--- day-23.py
+++ day-23.py
@@ -7,15 +7,130 @@ from pprint import pprint
 
 @dataclass
 class Node:
+    prev: int
+    next: int
+
+class CrabCups:
+    """ Extremely cursed data structure.
+    Here we implement a Circular Doubly Linked List as a flat array, taking advantage of the fact
+    that the values (cup labels) are sequential integers. Nodes are stored in a flat array, and
+    the index of the node is its label. So here `self.c[3]` contains the prev/next pointers for
+    the cup labeled `3` (where the pointers are just more integer indexes, not actual object
+    pointers/references). """
+
+    def __init__(self, labels, max_label):
+        # index 0 of this list just won't be used so I don't have to index with -1 everywhere
+        self.max_label = max_label
+        self.c = [None] * (len(labels) + 1)
+        last = labels[0]
+        self.c[last] = Node(None, None)
+        for i in range(1, len(labels)):
+            l = labels[i]
+            self.c[l] = Node(last, None)
+            self.c[last].next = l
+            last = l
+        self.c[labels[-1]].next = labels[0]
+        self.c[labels[0]].prev = last
+        self.head = labels[0]
+
+    def iter(self):
+        """ generator that yields the cups in order """
+        first = True
+        current = self.head
+        while current != self.head or first:
+            yield current
+            current = self.c[current].next
+            first = False
+
+    def fmt(self):
+        return ''.join(str(c) for c in self.iter())
+
+    def lsub(self, l):
+        if l == 1:
+            return self.max_label
+        elif l > 1:
+            return l - 1
+        else:
+            raise ValueError(f'Invalid cup label {l}')
+
+    def crab_move(self):
+        # slice of 3 cups after head
+        s1 = self.c[self.head].next
+        s2 = self.c[s1].next
+        s3 = self.c[s2].next
+
+        # remove them from the list
+        self.c[self.head].next = self.c[s3].next
+        self.c[self.c[self.head].next].prev = self.head
+
+        # find the destination
+        dest = self.head
+        while True:
+            dest = self.lsub(dest)
+            if dest not in (s1, s2, s3):
+                break
+
+        # insert the slice after the dest
+        destn = self.c[dest].next
+        self.c[dest].next = s1
+        self.c[s1].prev = dest
+        self.c[s3].next = destn
+        self.c[destn].prev = s3
+
+        # move to the new head
+        self.head = self.c[self.head].next
+
+def part_1(data):
+    labels = [int(x) for x in data.strip()]
+    cups = CrabCups(labels, 9)
+    print(f'{cups.c=}')
+    print(f'Initial state: {cups.fmt()}')
+
+    # do 100 moves
+    for turn in range(100):
+        cups.crab_move()
+        print(f'Turn {turn+1}: {cups.fmt()}')
+
+    # force cup 1 to be current
+    cups.current = 1
+    order = cups.fmt()
+    return order[1:]
+
+def part_2(data):
+    labels = [int(x) for x in data.strip()]
+    labels.extend(range(10, 1_000_001))
+    cups = CrabCups(labels, 1_000_000)
+
+    for turn in range(10_000_000):
+        if turn % 100_000 == 0:
+            print(f'{turn=}')
+        cups.crab_move()
+
+    a = cups.c[1].next
+    b = cups.c[a].next
+    return a * b
+
+FORMAT_1 = 'Cups after 1 = {}'
+FORMAT_2 = 'Multipled labels of two cups after 1 = {}'
+
+TEST_CASE_1 = [('389125467', '67384529')]
+TEST_CASE_2 = [('389125467', 149245887792)]
+
+
+# SLOW VERSION
+# Uses a comprehensible linked list, but is too slow for Part 2
+
+@dataclass
+class _Node:
     label: int
     prev: object
     next: object
 
 def make_ring(labels):
-    head = Node(labels[0], None, None)
+    head = _Node(labels[0], None, None)
     last = head
     for l in labels[1:]:
-        new = Node(l, last, None)
+        new = _Node(l, last, None)
         last.next = new
         last = new
     new.next = head
@@ -84,7 +199,7 @@ def crab_move(head, max_val=9):
 def fmt_list(head):
     return ' '.join(map_each(head, lambda node: str(node.label)))
 
-def part_1(data):
+def _part_1(data):
     labels = [int(x) for x in data.strip()]
     head = make_ring(labels)
     print(f'Initial state: {fmt_list(head)}')
@@ -100,7 +215,7 @@ def part_1(data):
     # but don't actually print the '1'
     return order[1:]
 
-def part_2(data):
+def _part_2(data):
     labels = [int(x) for x in data.strip()]
     labels.extend(range(10, 1_000_001))
     head = make_ring(labels)
@@ -113,9 +228,3 @@ def part_2(data):
     head1 = find_node(head, lambda node: node.label == 1)
     print('cups after 1: {head1.next.label}, {head1.next.next.label}')
     return head1.next.label * head1.next.next.label
-
-FORMAT_1 = 'Cups after 1 = {}'
-FORMAT_2 = 'Multipled labels of two cups after 1 = {}'
-
-TEST_CASE_1 = [('389125467', '67384529')]
-TEST_CASE_2 = [('389125467', 149245887792)]
